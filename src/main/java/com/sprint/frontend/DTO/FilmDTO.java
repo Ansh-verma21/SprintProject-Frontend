13f5bb72b9fb9682eb9dd50add4162fac91d5ee1
Ansh-verma21 <vermaansh13579@gmail.com>
2026-05-04 20:53:50 +0530

    dto film page 2

--- src/main/java/com/sprint/frontend/DTO/FilmDTO.java
+++ src/main/java/com/sprint/frontend/DTO/FilmDTO.java
@@ -2,4 +2,26 @@ package com.sprint.frontend.DTO;
 
 public class FilmDTO {
 
+    private Long filmId;
+    private String title;
+    private String description;
+    private String releaseYear;
+    private String rating;
+
+    public FilmDTO() {}
+
+    public Long getFilmId() { return filmId; }
+    public void setFilmId(Long filmId) { this.filmId = filmId; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public String getReleaseYear() { return releaseYear; }
+    public void setReleaseYear(String releaseYear) { this.releaseYear = releaseYear; }
+
+    public String getRating() { return rating; }
+    public void setRating(String rating) { this.rating = rating; }
 }
